8e58b61b13a0894158b7b570199768479aadd3f2
EdiBOI25 <jaguarul2501@gmail.com>
2024-01-03 18:34:35 +0200

    solved pb10 of lab12

--- 12/main.c
+++ 12/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
+int find_substring(char substring[], char otherstring[]);
+
 int main() {
-	char hw_string[] = "Hello World!"
-	printf("%s", hw_string);
+	int n = 4;
+	char substring[100];
+	char other_strings[20][100];
+
+	printf("Enter substring: ");
+	scanf("%s", substring);
+	for (int i = 0; i < n; ++i) {
+		// Read other strings
+		printf("Enter string %d: ", i + 1);
+		scanf("%s", other_strings[i]);
+	}
+
+	int found = 1;
+	// Check if substring is in other strings
+	for (int i = 0; i < n; ++i) {
+		found = find_substring(substring, other_strings[i]);
+		printf("Found = %d\n", found);
+		if (found == 0) {
+			printf("Substring not found in string %d.", i + 1);
+			return 0;
+		}
+	}
+
+	if (found == 1) {
+		printf("Substring found in every other string");
+	}
+
+	return 0;
 }
--- 12/modul.asm
+++ 12/modul.asm
@@ -1,3 +1,74 @@
 bits 32
-segment data use32
-segment code use32
+
+global _find_substring
+extern _printf
+
+segment data use32 public data
+    substring_address dd -1
+    otherstring_address dd -1
+    length_sub dd -1
+    
+segment code use32 public code
+; find_substring(substring, otherstring)
+_find_substring:
+    push ebp
+    mov ebp, esp
+    sub esp, 12 ; rezervam 4*3 octeti in stiva pt variabilele locale
+    
+    mov eax, [ebp + 8] ; substring
+    mov [substring_address], eax
+    mov eax, [ebp + 12] ; otherstring
+    mov [otherstring_address], eax
+    
+    
+    ; find length of substring
+    mov eax, 0
+    mov esi, [substring_address]
+    substr_len:
+        cmp byte [esi], 0
+        je after_substr_len
+        add eax, 1
+        add esi, 1
+        jmp substr_len
+        
+    after_substr_len:
+        mov [length_sub], eax
+        
+        
+    ; searches in segment of string2 starting from edx
+    mov edx, [otherstring_address]
+    search:
+        cmp byte [edx], 0 ; check if we are at the end of string2
+        je substring_not_found
+        mov esi, [substring_address] ; esi = address of string1
+        mov edi, edx ; edi = address of string2 starting from the character pointed by edx
+        mov ecx, 0
+        mov ecx, [length_sub]
+        jecxz after_sub_loop
+        sub_loop:
+            mov ebx, [esi]
+            mov eax, [edi]
+            cmp al, bl ; check if characters are equal
+            jne after_sub_loop
+            add esi, 1
+            add edi, 1
+            loop sub_loop
+        jmp substring_found
+        after_sub_loop:
+        add edx, 1 ; start from next character
+        jmp search
+    
+    substring_found:
+        mov eax, 1
+        jmp final
+        
+    substring_not_found:
+        mov eax, 0
+    
+
+
+final:
+    add esp, 12
+    mov esp, ebp
+    pop ebp
+    ret
